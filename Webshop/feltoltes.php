38de9997ae55dbaad3576fb3aae4ef6052fcc382
DrSeres <112695233+DrSeres@users.noreply.github.com>
2023-03-03 20:35:42 +0100

    Update

--- Webshop/feltoltes.php
+++ Webshop/feltoltes.php
@@ -8,9 +8,4 @@ $prices = $_POST['prices'];
 $sql = "INSERT INTO `megrendeles`(`id`, `nev`, `price`, `darab`) VALUES ('','{$nev}','{$prices}','{$rendelesDb}')";
 $eredmeny = mysqli_query($dbconnect, $sql);
 
-// if(mysqli_num_rows($eredmeny) > 1)
-// {
-//     $sql = "UPDATE `megrendeles` SET `nev`='{$nev}',`price`='{$prices}',`darab`='{$rendelesDb}'";
-// }
-echo $eredmeny;
 ?>
