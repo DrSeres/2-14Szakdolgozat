ed0e621785cb71f2ac52b40fe30c2b9ef45c0c6c
DrSeres <112695233+DrSeres@users.noreply.github.com>
2023-03-05 10:43:52 +0100

    Megrendelés tábla email kiolvasása

--- Webshop/feltoltes.php
+++ Webshop/feltoltes.php
@@ -1,11 +1,27 @@
 <?php
 
+
 require("../Webshop/kapcsolat.php");
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $nev = $_POST["nev"];
 $rendelesDb = $_POST['rendelesDb'];
 $prices = $_POST['prices'];
 
-$sql = "INSERT INTO `megrendeles`(`id`, `nev`, `price`, `darab`) VALUES ('','{$nev}','{$prices}','{$rendelesDb}')";
+
+
+$select = "SELECT email FROM users WHERE name = '{$_SESSION['name']}'";
+print($select);
+
+$result = mysqli_query($dbconnect, $select);
+$eredmeny = mysqli_fetch_array($result);
+print_r($eredmeny);
+$email = $eredmeny['email'];
+print_r($email);
+
+$sql = "INSERT INTO `megrendeles`(`id`, `emailNev`, `nev`, `price`, `darab`) VALUES ('','{$email}', '{$nev}','{$prices}','{$rendelesDb}')";
 $eredmeny = mysqli_query($dbconnect, $sql);
 
 ?>
