a82bcdb7d3692423e20f60f6e87084222c620a7b
DrSeres <112695233+DrSeres@users.noreply.github.com>
2023-03-09 09:28:12 +0100

    Update index.php

--- Webshop/index.php
+++ Webshop/index.php
@@ -211,52 +211,52 @@ session_start();
         </figcaption>
       </div>
       <div class="hover-img oszlop2">
-        <a href="asd"><img src="img/procHuto.jpg" alt="" width="100%"></a>
+        <a href="asd"><img src="../img/procHuto.jpg" alt="" width="100%"></a>
         <figcaption>
           <a href="processzorHuto.php"><h3>Processzorhűtő</h3></a>
         </figcaption>
       </div>
       <div class="hover-img oszlop3">
-        <a href="asd"><img src="img/alaplap.jpg" alt="" width="100%"></a>
+        <a href="asd"><img src="../img/alaplap.jpg" alt="" width="100%"></a>
         <figcaption>
           <a href="alaplap.php"><h3>Alaplap</h3></a>
         </figcaption>
       </div>
       <div class="hover-img oszlop4">
-        <a href="asd"><img src="img/videokartya.jpg" alt="" width="100%"></a>
+        <a href="asd"><img src="../img/videokartya.jpg" alt="" width="100%"></a>
         <figcaption>
           
           <a href="videokartya.php"><h3>Videókártya</h3></a>
         </figcaption>
       </div>
       <div class="hover-img oszlop5">
-        <a href="asd"><img src="img/memoria.jpg" alt="" width="100%" style="background: transparent;"></a>
+        <a href="asd"><img src="../img/memoria.jpg" alt="" width="100%" style="background: transparent;"></a>
         <figcaption>
           
           <a href="ram.php">Memória</h3></a>
         </figcaption>
       </div>
       <div class="hover-img oszlop6">
-        <a href="asd"><img src="img/rendszerhuto.jpg" alt="" width="100%"></a>
+        <a href="asd"><img src="../img/rendszerhuto.jpg" alt="" width="100%"></a>
         <figcaption>
           <a href="rendszerHuto.php"><h3>Rendszerhűtő</h3></a>
         </figcaption>
       </div>
       <div class="hover-img oszlop7">
-        <a href="asd"><img src="img/ssd.jpg" alt="" width="100%"></a>
+        <a href="asd"><img src="../img/ssd.jpg" alt="" width="100%"></a>
         <figcaption>
           <a href="ssd.php"><h3>SSD</h3></a>
         </figcaption>
       </div>
       <div class="hover-img oszlop8">
-        <img src="img/tapegyseg.jpg" alt="" width="100%">
+        <img src="../img/tapegyseg.jpg" alt="" width="100%">
         <figcaption>
           <a href="tapegyseg.php"><h3>Tápegység</h3></a>
           
         </figcaption>
       </div>
       <div class="hover-img oszlop9">
-        <a href="gephaz.php"><img src="img/gephaz.jpg" alt="" width="100%"></a>
+        <a href="gephaz.php"><img src="../img/gephaz.jpg" alt="" width="100%"></a>
         <figcaption>
           <a href="gephaz.php"><h3>Számítógépház</h3></a>
         </figcaption>
