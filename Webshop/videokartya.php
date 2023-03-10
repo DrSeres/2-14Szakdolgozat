330cc9e7786b2742b6088b1e485b81cbbab2cf27
DrSeres <112695233+DrSeres@users.noreply.github.com>
2023-03-10 08:49:38 +0100

    Lapozó

--- Webshop/videokartya.php
+++ Webshop/videokartya.php
@@ -4,23 +4,64 @@ session_start();
 $sql = "SELECT * FROM kategoria INNER JOIN gyarto ON kategoria.kategoriaID=gyarto.kategoriaID INNER JOIN termek ON gyarto.gyartoId=termek.markaId WHERE kategoria.kategoriaID = 4;";
 $eredmeny = mysqli_query($dbconnect, $sql);
 
-$osszes   = mysqli_num_rows($eredmeny);
-$mennyit  = 9;
-$lapok    = ceil($osszes / $mennyit); 
+//szükséges adatok a számításhoz
+$mennyit = 8; //ennyi kártyát akarok látni egy oldalon
+$osszesen = mysqli_num_rows($eredmeny);
+//print_r($osszesen);
+
+$lapok = ceil($osszesen / $mennyit); //kerekítés
+//print($lapok);
+
 $aktualis = (isset($_GET['oldal'])) ? (int)$_GET['oldal'] : 1;
-$honnan   = ($aktualis-1)*$mennyit; 
 
-$kifejezes = (isset($_GET['kifejezes'])) ? $_GET['kifejezes'] : "";
+/** 
+ * 0, 10 - gép számítása 0-tól 1-oltal
+ * 10, 10
+ * 20, 10 - 3ik oldal
+*/
 
-// $sql = "SELECT *
-//              FROM videokartya
-//             WHERE (
-// 			modell LIKE '%{$kifejezes}%' 
-// 		)
-//             ORDER BY ar ASC
-//             LIMIT {$honnan}, {$mennyit}";       
-// $eredmeny = mysqli_query($dbconnect, $sql);
+$honnan = ($aktualis -1) * $mennyit >= 1 ? ($aktualis -1) * $mennyit : 1;
+//print_r($honnan);
 
+
+//lapozó felépítése, hivatkozásoknak kell lennie
+$lapozo = "<nav data-pagination>";
+$lapozo .= "<ul class='pagination'>";
+$lapozo .= ($aktualis != 1) ? "<a href=\"?oldal=1\">< Első</a> |" : "< Első |";
+
+$lapozo .= ($aktualis > 1 && $aktualis <= $lapok) ? "<li><a href=\"?oldal=" .($aktualis-1)."\">Előző</a> | " : "Előző |";
+
+//össszes oldalra el kell végezni a vizsgálatot, amin állsz az legyen link
+
+for ($oldal=1; $oldal <= $lapok; $oldal++) { 
+    $lapozo .= ($aktualis != $oldal) ? "<li><a href=\"?oldal={$oldal}\">{$oldal}</a> | " : $oldal." |";
+}
+
+$lapozo .= ($aktualis > 0 && $aktualis < $lapok) ? "<a href=\"?oldal=".($aktualis + 1)."\">Következő</a> | " : "Következő | ";
+
+$lapozo .= ($aktualis != $lapok) ? "<li><a href=\"?oldal=".($lapok)."\">Utolsó</a> >" : "Utolsó >";
+
+$lapozo .= "</ul>";
+$lapozo .= "</nav>";
+
+
+
+
+
+
+
+$sql = "SELECT * FROM kategoria INNER JOIN gyarto ON kategoria.kategoriaID=gyarto.kategoriaID INNER JOIN termek ON gyarto.gyartoId=termek.markaId WHERE kategoria.kategoriaID = 4 ORDER BY gyarto.gyartoId ASC 
+LIMIT {$honnan}, {$mennyit}";
+$eredmeny = mysqli_query($dbconnect, $sql);
+
+if((mysqli_num_rows($eredmeny)) < 1)
+{
+    $kimenet = "<article>
+    <h2>Nincs találat a rendszerben</h2>
+    </article>";
+}
+else
+{
 $kimenet = "";
 while ($sor = mysqli_fetch_assoc($eredmeny)) {
     $kimenet .=
@@ -47,7 +88,7 @@ while ($sor = mysqli_fetch_assoc($eredmeny)) {
 </article>
 URLAP;
 }
-
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -147,6 +188,7 @@ URLAP;
                 
                 ?>
                 </div>
+                <?php print $lapozo; ?>
         </main>
     
     <!--Kosár tartalma-->
