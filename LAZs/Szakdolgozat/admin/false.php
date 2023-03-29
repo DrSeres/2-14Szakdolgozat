c3075f29218562760eeb4376b7f10c5e38ef50d8
L-Adrian-Zs <112695343+L-Adrian-Zs@users.noreply.github.com>
2023-03-29 09:14:32 +0200

    Kitiltottak oldala

--- LAZs/Szakdolgozat/admin/false.php
+++ LAZs/Szakdolgozat/admin/false.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/false.css">
-    <link rel="stylesheet" href="css/button.css">
     <title>FIGYELMEZTETÉS!</title>
 </head>
 <body>
