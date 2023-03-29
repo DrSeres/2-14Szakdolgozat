3d7fd4eb85a811f24b950aa61acf0c5435b9c65a
L-Adrian-Zs <112695343+L-Adrian-Zs@users.noreply.github.com>
2023-03-29 10:23:05 +0200

    Részletek oldal

--- LAZs/Szakdolgozat/admin/reszletek.php
+++ LAZs/Szakdolgozat/admin/reszletek.php
@@ -4,9 +4,9 @@
 
 require("kapcsolat.php"); 
 
-if(!isset($_SESSION['user_type']) == 'admin'){
-  header("location:false.php");
-}
+// if(!isset($_SESSION['user_type']) == 'admin'){
+//   header("location:false.php");
+// }
 
 
 
@@ -19,13 +19,18 @@ if(!isset($_SESSION['user_type']) == 'admin'){
 $id = (int)$_GET['id'];
 $sql = "SELECT*
         FROM termek
+        INNER JOIN gyartokategoria ON termek.gyartoKategoriaId=gyartokategoria.gyartoKategoriaId
+        INNER JOIN gyarto ON gyartokategoria.gyartoId=gyarto.gyartoId
+        INNER JOIN kategoria ON gyartokategoria.kategoriaID=kategoria.kategoriaID
         WHERE id = {$id}";
 $eredmeny = mysqli_query($dbconn, $sql);
 $sor = mysqli_fetch_assoc($eredmeny);
 
-
+$cim = $sor['termekNev'];
+$termek = $sor['gyartoNev'] . " " . $sor['termekNev'];
 $leiras = $sor['leiras'];
 $ar = $sor['ar'];
+$foto = $sor['foto'];
 
 
 
@@ -40,6 +45,7 @@ $ar = $sor['ar'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/felv_mod.css">
+    <link rel="stylesheet" href="css/reszletei.css">
     <link rel="stylesheet" href="css/table.css">
     <title> <?php print $cim ?></title>
     <script src="ckeditor.js"></script>
@@ -47,16 +53,13 @@ $ar = $sor['ar'];
 
 <body>
     <div class="container">
-        
+        <h1><?php print $termek ?> adatai</h1>
+        <img src="../img/termekekuj/<?php print $foto;?>" alt="">
         <form method="post" enctype="multipart/form-data">
-
-           <textarea name="leiras" id="editor" cols="30" rows="10"><?php print $leiras;?></textarea>
+            <?php print $leiras; ?>
             <p><a href="kategoria.php">Vissza az oldalra</a></p>
         </form>
     </div>
-<script>
-    CKEDITOR.replace('editor');
-</script>
 </body>
 
 </html>
